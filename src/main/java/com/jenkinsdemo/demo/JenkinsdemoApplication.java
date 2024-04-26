94a3485a8a6ed241fb6da94ee7b301b2dabab655
prashanthan-v <prashanthansiva24@gmail.com>
2024-04-26 19:07:16 +0530

    first commit

---
+++ src/main/java/com/jenkinsdemo/demo/JenkinsdemoApplication.java
@@ -0,0 +1,15 @@
+package com.jenkinsdemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemoApplication.class, args);
+		System.out.println("appliction is build");
+	}
+
+
+}
---
+++ src/test/java/com/jenkinsdemo/demo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jenkinsdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("testing worked!!");
+	}
+
+}
