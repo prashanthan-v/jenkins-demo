50dfe560de43eed6cec533c5b4ecbdd5c67a6bf5
prashanthan-v <prashanthansiva24@gmail.com>
2024-04-27 13:15:40 +0530

    third commit without dependency graph

--- src/main/java/com/jenkinsdemo/demo/JenkinsdemoApplication.java
+++ src/main/java/com/jenkinsdemo/demo/JenkinsdemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsdemoApplication {
 
-	public static void main(String[] args) {
+	public statc void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 		System.out.println("appliction is build");
 	}
