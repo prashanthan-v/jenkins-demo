47d60917a2f5db12cf9f0b87a081de311dff4dab
prashanthan-v <prashanthansiva24@gmail.com>
2024-04-27 18:52:16 +0530

    using dockerfile instead of jib

--- src/main/java/com/jenkinsdemo/demo/JenkinsdemoApplication.java
+++ src/main/java/com/jenkinsdemo/demo/JenkinsdemoApplication.java
@@ -2,13 +2,20 @@ package com.jenkinsdemo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 		System.out.println("appliction is build");
+	}
+	@GetMapping("/home")
+	public String welcome(){
+		return "hello jib is working";
 	}
 
 
